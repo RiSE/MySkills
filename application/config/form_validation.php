083436f0b0c4a06ec72bbed7c616a36eb8e26ea0
Tiago Perrelli <tiagoyg@gmail.com>
2012-06-21 17:00:58 -0300

    update
    form validation

--- application/config/form_validation.php
+++ application/config/form_validation.php
@@ -4,36 +4,52 @@ if (!defined('BASEPATH'))
     exit('No direct script access allowed');
 
 /**
- * Naips Tecnoogia
- * 
- * Medical Software Technology 
- * 
- * @package		Naips
+ *
+ * @package		MySkills
  * @author		Tiago Perrelli
- * @copyright           Copyright (c) 2012, NAIPS, Inc.
+ * @copyright           Copyright (c) 2012, MySkills, Inc.
  * @license		
- * @link		http://www.naips.com.br
  * @since		2012
  * @filesource
  * @version             0.1
  */
 $config = array(
-    
     'programmer' => array(
         array(
             'field' => 'code',
             'label' => 'Code',
+<<<<<<< HEAD
             'rules' => 'numeric'
+=======
+            'rules' => 'numeric|max_length[30]'
+>>>>>>> update
         ),
         array(
             'field' => 'email',
             'label' => 'Email',
-            'rules' => 'required|valid_email'
+            'rules' => 'required|valid_emailmax_length[255]'
         ),
         array(
             'field' => 'selectUf',
             'label' => 'UF',
-            'rules' => 'required|numeric'
+            'rules' => 'numeric|max_length[2]'
+        )
+    ),
+    'recruiter' => array(
+        array(
+            'field' => 'company',
+            'label' => 'Company',
+            'rules' => 'required|max_length[100]'
+        ),
+        array(
+            'field' => 'email',
+            'label' => 'Email',
+            'rules' => 'required|valid_email|max_length[255]'
+        ),
+        array(
+            'field' => 'selectUf',
+            'label' => 'UF',
+            'rules' => 'numeric|max_length[2]'
         )
     )
 );
