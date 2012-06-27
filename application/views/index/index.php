4547ec290e00624910b2373c0d8af31926402770
Eduardo Cruz <eduardo.cruz@rise.com.br>
2012-06-27 09:49:24 -0300

    Changing information on the front page to improve usar activation.

--- application/views/index/index.php
+++ application/views/index/index.php
@@ -120,17 +120,16 @@
                         <div class="span6 landing-text">
 
 
-                            <h1>The best way to hire programmers in Brazil.</h1>
-                            <h2>A badge ranking system.</h2>
+                            <h1>The best way to hire <br/>developers in Brazil.</h1>
+                            <h2>A software developer ranking system.</h2>
 
                             <p>If you are a recruiter, reach skilled developers in a categorized validated environment.</p>
                             <p>If you are a programmer, become a better coder and get a better job or even a promotion in your actual job.</p>
 
                             <p class="landing-actions">
-                                <a href="<?php echo base_url(); ?>index/recruiters" class="btn btn-small btn-inverse">Recruiters - R$ 25,00</a>
-                                <a href="<?php echo base_url(); ?>index/programmers" class="btn btn-small">Programmers - Free</a>
+                                <a href="http://www.myskills.com.br/index/recruiters" class="btn btn-small btn-primary">Recruiter - Sign-up here</a>
+                                <a href="http://www.myskills.com.br/index/programmers" class="btn btn-warning">Developer - Sign-up here</a>
                             </p>		
-
 
                         </div> <!-- /landing-text -->							
 
