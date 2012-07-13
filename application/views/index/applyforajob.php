0e0353a5220d05ecc05e800c3228ed1a656bb5e6
Tiago Perrelli <tiagoyg@gmail.com>
2012-07-13 08:52:03 -0300

    commit
    add changes 2012-07-13

---
+++ application/views/index/applyforajob.php
@@ -0,0 +1,77 @@
+<script type="text/javascript">
+    mixpanel.track("Apply for a Job");
+    
+    $(document).ready(function() {
+        //$('#myModal').modal('toggle');
+    });
+</script>
+
+<div id="subheader">
+    <div class="inner">
+        <div class="container">
+            <h1><?php echo $title; ?></h1>
+        </div>
+    </div>
+</div>
+
+<!--<a class="btn" id="myModal" data-toggle="modal" href="#myModal" >Launch Modal</a>-->
+
+<div id="subpage">	
+    <div class="container">
+
+        <div class="row">
+
+            <div class="span2">
+                <ul class="nav nav-tabs nav-stacked">
+                    <li class="">
+                        <a href="<?php echo base_url(); ?>professional/applyforajob?type=web">Web Development</a>
+                    </li>
+                    <li>
+                        <a href="<?php echo base_url(); ?>professional/applyforajob?type=mobile">Mobile Development</a>
+                    </li>
+                </ul>
+
+                <!--<div class="tabbable tabs-left">
+                    <ul class="nav nav-tabs">
+                      <li class="active">
+                          <a href="#lA" data-toggle="tab">Section 1</a>
+                      </li>
+                    </ul>
+                    <div class="tab-content">
+                      
+                    </div>
+                </div>-->
+            </div>
+
+            <div class="span10">
+                <div class="span8 sidebar ">
+                    <form method="POST" name="frmJob" action="<?php echo base_url(); ?>index/apply">
+                        <?php foreach ($jobs as $job) : ?>
+
+                            <div class="span6">
+                                <p>
+                                    <?php echo $job->title; ?>
+                                </p>
+                            </div>
+
+                            <?php $disabled = ''; ?>
+                            <?php foreach ($applieds as $applied) : ?>
+                                <?php if ($job->id_job == $applied->id_job) : ?>
+                                    <?php $disabled = 'disabled="disabled"'; ?>
+                                <?php endif; ?>
+                            <?php endforeach; ?>
+
+                            <div class="span1">
+                                <input type="submit" <?php echo $disabled; ?> class="btn btn-primary btn-large" value="Apply" />
+                            </div>                        
+
+                            <input type="hidden" name="ids" value="<?php echo $job->id_job; ?>" />
+                        <?php endforeach; ?>
+                    </form>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+</div>
---
+++ application/views/index/profile.php
@@ -0,0 +1,214 @@
+<script type="text/javascript">
+    mixpanel.track("Professional Profile");
+</script>
+
+<div id="subheader">
+    <div class="inner">
+        <div class="container">
+            <h1><?php echo $title; ?></h1>
+        </div>
+    </div>
+</div>
+
+<div id="subpage">	
+    <div class="container">
+
+        <form method="POST" name="frmClaim">
+
+            <!--            <div class="container">-->
+
+            <div class="row">
+                <!-- -->
+                <div class="span2" name="divProfile">
+                    <img id="userpic" src="https://graph.facebook.com/<?php echo $this->session->userdata('uid'); ?>/picture&type=normal" style="border:thick groove green;" />
+                    <br /><br />
+
+                    <img src="<?php echo base_url(); ?>assets/images/badges/unlock100.png" width="65"></img>
+                    <img src="<?php echo base_url(); ?>assets/images/badges/unlock100.png" width="65"></img>
+                    <img src="<?php echo base_url(); ?>assets/images/badges/unlock100.png" width="65"></img>
+                    <img src="<?php echo base_url(); ?>assets/images/badges/unlock100.png" width="65"></img>
+                    <img src="<?php echo base_url(); ?>assets/images/badges/unlock100.png" width="65"></img>
+                    <img src="<?php echo base_url(); ?>assets/images/badges/unlock100.png" width="65"></img>
+                    <img src="<?php echo base_url(); ?>assets/images/badges/unlock100.png" width="65"></img>
+                    <img src="<?php echo base_url(); ?>assets/images/badges/unlock100.png" width="65"></img>                        
+                </div>
+
+                <div class="span10" name="divContainer">
+
+                    <h3 id="hname"><span class="slash">Professional:</span></h3>
+
+                    <div class="row">
+                        <div class="span8">
+                            &nbsp;
+                        </div>
+                    </div>
+
+                    <!--<div class="span2"><a href="#" class="btn btn-large btn disabled">Skill Points</a></div>
+                    <div class="span2"><a href="#" class="btn btn-large btn disabled">Job Offers</a></div>
+                    <div class="span2"><a href="#" class="btn btn-large btn disabled">Job Applications</a></div>
+                    <div class="span2"><a href="#" class="btn btn-large btn disabled">Course Savings</a></div>-->
+
+                    <div class="row">
+                        <div class="span8">
+                            &nbsp;
+                        </div>
+                    </div>
+
+                    <!--<div class="span2"><a href="#" class="btn btn-large btn disabled">Badges</a></div>
+                    <div class="span2"><a href="#" class="btn btn-large btn disabled">Courses</a></div>
+                    <div class="span2"><a href="#" class="btn btn-large btn disabled">Profile Views</a></div>
+                    <div class="span2"><a href="#" class="btn btn-large btn disabled">Expected Salary</a></div>-->
+
+                    <div class="row">
+                        <div class="span8">
+                            &nbsp;
+                        </div>
+                    </div>
+
+                    <div class="span3">
+                        <a href="<?php echo base_url(); ?>index/applyforajob" class="btn btn-success btn-large">Apply for a Job</a>
+                    </div>
+                    <div class="span3">
+                        <a href="<?php echo base_url(); ?>index/claimbadges" class="btn btn-warning btn-large">Claim your Badges</a>
+                    </div>
+                    <div class="span3">
+                        <a class="btn btn-success btn-large" disabled="disabled">Apply for a Course</a>
+                    </div>
+
+                    <div class="row">
+                        <div class="span8">
+                            &nbsp;
+                        </div>
+                    </div>
+
+                    <!--<div class="span8 sidebar">
+                        <p></p>
+                    </div>-->
+
+                    <div class="row">
+                        <div class="span8">
+                            &nbsp;
+                        </div>
+                    </div>
+
+
+                    <div class="span8  sidebar">
+                        <h2>News</h2>
+                        <p></p>
+                    </div>
+
+                </div>
+
+            </div>
+
+            <!--            </div>-->
+
+            <!--<div class="row">
+        
+                <div class="span2">
+                    <img id="userpic" src="https://graph.facebook.com/<?php echo $this->session->userdata('uid'); ?>/picture&type=normal" style="border:thick groove green;" />
+                    <br /><br />
+        
+                    <img src="<?php echo base_url(); ?>assets/images/badges/unlock100.png" width="65"></img>
+                    <img src="<?php echo base_url(); ?>assets/images/badges/unlock100.png" width="65"></img>
+                    <img src="<?php echo base_url(); ?>assets/images/badges/unlock100.png" width="65"></img>
+                    <img src="<?php echo base_url(); ?>assets/images/badges/unlock100.png" width="65"></img>
+                    <img src="<?php echo base_url(); ?>assets/images/badges/unlock100.png" width="65"></img>
+                    <img src="<?php echo base_url(); ?>assets/images/badges/unlock100.png" width="65"></img>
+                    <img src="<?php echo base_url(); ?>assets/images/badges/unlock100.png" width="65"></img>
+                    <img src="<?php echo base_url(); ?>assets/images/badges/unlock100.png" width="65"></img>
+        
+                </div>
+        
+                <div class="span10">
+                    <h3 id="hname"><span class="slash">Professional:</span></h3>
+                    <hr>
+        
+                    <div class="span2"><a href="#" class="btn btn-large btn disabled">Skill Points</a></div>
+                    <div class="span2"><a href="#" class="btn btn-large btn disabled">Job Offers</a></div>
+                    <div class="span2"><a href="#" class="btn btn-large btn disabled">Job Applications</a></div>
+                    <div class="span2"><a href="#" class="btn btn-large btn disabled">Course Savings</a></div>
+        
+                    <br /><br /><br />
+        
+                    <div class="span2"><a href="#" class="btn btn-large btn disabled">Badges</a></div>
+                    <div class="span2"><a href="#" class="btn btn-large btn disabled">Courses</a></div>
+                    <div class="span2"><a href="#" class="btn btn-large btn disabled">Profile Views</a></div>
+                    <div class="span2"><a href="#" class="btn btn-large btn disabled">Expected Salary</a></div>
+                </div>
+        
+                <br /><br /><br />
+        
+                <br /><br /><br />
+        
+                <div class="span10">
+                    <a class="btn btn-primary btn-large">Learn more</a>
+                    <a class="btn btn-primary btn-large">Learn more</a>
+                    <a class="btn btn-primary btn-large">Learn more</a>
+                    <a class="btn btn-primary btn-large">Learn more</a>
+                    <a class="btn btn-primary btn-large">Learn more</a>
+                </div>
+        
+        
+            </div>-->
+
+            <!--<div class="row-fluid">
+        
+                <div class="span12">
+        
+                    <img id="userpic" src="https://graph.facebook.com/<?php echo $this->session->userdata('uid'); ?>/picture&type=normal" style="border:thick groove green;" />
+        
+                    <img src="<?php echo base_url(); ?>assets/images/badges/unlock100.png" width="100"></img>
+                    <img src="<?php echo base_url(); ?>assets/images/badges/unlock100.png" width="100"></img>
+                    <img src="<?php echo base_url(); ?>assets/images/badges/unlock100.png" width="100"></img>
+                    <img src="<?php echo base_url(); ?>assets/images/badges/unlock100.png" width="100"></img>
+                    <img src="<?php echo base_url(); ?>assets/images/badges/unlock100.png" width="100"></img>
+        
+                    <div class="row-fluid">
+                        <div class="span9">
+                            <hr>
+                            <h3 id="hname"><span class="slash">Professional:</span></h3>
+                            <hr>
+                            <span class="slash">Badges:</span>
+                            <select name="selectBadges">
+                                <option value="">--SELECT--</option>
+            <?php foreach ($badges as $badge) : ?>
+                                                                                                                                                                            <option value="<?php echo $badge->id_badge; ?>"><?php echo $badge->name; ?></option>
+            <?php endforeach; ?>
+                            </select>
+            <?php echo form_error('selectBadges'); ?>
+                            <input type="text" id="code" name="code" placeholder="Type Your Code Certificate" />
+            <?php echo form_error('code'); ?>
+        
+                            <input type="submit" value="Claim Right now!" name="claim" />
+            <?php echo $badge_error; ?>
+                        </div>
+                        <div class="span3">
+                            <div class="sidebar">
+                                <h4><span class="slash">//</span>iOS Training Badge</h4>
+                                <img src="<?php echo base_url(); ?>assets/images/badges/iOSBadge100.png" width="50" align="left" />
+        
+                                <p>
+                                    The iOS badge is provided for students that participated on an 
+                                    Apple technology trainning. This course provided content and 
+                                    practice in iOS application development including: iPad, iPhone and iPod touch.
+                                    Unlock Badge.
+                                </p>
+        
+                                <h4><span class="slash">//</span>The Unlock Badge</h4>
+                                <img src="<?php echo base_url(); ?>assets/images/badges/unlock100.png" width="50" align="left"/>
+        
+                                <p>
+                                    The Unlock badge is used as a visual representation for programmers and 
+                                    recruiters that the programmer can unlock other badges in the future to improve his profile page.
+                                </p>
+        
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>-->
+
+        </form>
+    </div>
+</div>
