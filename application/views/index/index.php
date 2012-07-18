f3333a4f95df29e1abc6e77f398201943afe74b6
eliakim ramos de souza <eliakim.ramos@rise.com.br>
2012-07-18 13:39:42 -0300

    Commit
    Mudança na index

--- application/views/index/index.php
+++ application/views/index/index.php
@@ -42,6 +42,178 @@
 
 </div> <!-- /landing -->
 
+<div id="content"> 
+				
+	<div class="inner">		
+		
+		<div class="container">
+			
+			<div class="row">
+			
+				<div class="span8">
+				
+						<h3><span class="slash">//</span> What You'll Get</h3>
+						
+						
+						<ul class="features-list">
+							<li class="feature">
+								<div class="feature-thumbnail">
+									<div class="circle-icon">
+										<div>
+											<i class="icon-list-alt"></i>
+										</div>
+									</div> <!-- /circle-icon -->
+								</div> <!-- /feature-thumbnail -->
+								
+								<div class="feature-content">
+									<h3>Feature #1</h3>
+									<p>Nunc metus ligula, fringilla quis porta ut, tellus arcu  mi eget adipiscing dictum ut turpis. Aliquam eget odio.</p>									
+								</div> <!-- /feature-content -->
+							</li>
+							<li class="feature">
+								<div class="feature-thumbnail">
+									<div class="circle-icon">
+										<div>
+											<i class="icon-flag"></i>
+										</div>
+									</div> <!-- /circle-icon -->
+								</div> <!-- /feature-thumbnail -->
+								
+								<div class="feature-content">
+									<h3>Feature #2</h3>
+									<p>Vestibulum ante ipsum primis tellus arcu  mi eget adipiscing in faucibus orci luctus et ultrices posuere cubilia curae.</p>
+							</div> <!-- /feature-content -->
+							</li>
+							<li class="feature">
+								<div class="feature-thumbnail">
+									<div class="circle-icon">
+										<div>
+											<i class="icon-resize-small"></i>
+										</div>
+									</div> <!-- /circle-icon -->
+								</div> <!-- /feature-thumbnail -->
+								
+								<div class="feature-content">
+									<h3>Feature #3</h3>
+									<p>Aliquam justo massa, rhoncus quis tellus arcu  mi eget adipiscing suscipit eget, malesuada vel nulla. Sed ac nunc.</p>
+							</div> <!-- /feature-content -->
+							</li>
+							<li class="feature">
+								<div class="feature-thumbnail">
+									<div class="circle-icon">
+										<div>
+											<i class="icon-tint"></i>
+										</div>
+									</div> <!-- /circle-icon -->
+								</div> <!-- /feature-thumbnail -->
+								
+								<div class="feature-content">
+									<h3>Feature #4</h3>
+									<p>Phasellus mollis, tortor vitae ornare commodo, augue tellus arcu  mi eget adipiscing risus varius quam, sit amet.</p>
+							</div> <!-- /feature-content -->
+							</li>
+							<li class="feature">
+								<div class="feature-thumbnail">
+									<div class="circle-icon">
+										<div>
+											<i class="icon-trophy"></i>
+										</div>
+									</div> <!-- /circle-icon -->
+								</div> <!-- /feature-thumbnail -->
+								
+								<div class="feature-content">
+									<h3>Feature #5</h3>
+									<p>Sed feugiat pellentesque tellus arcu  mi eget adipiscing dui, laoreet aliquet enim condimentum quis commodo.</p>
+							</div> <!-- /feature-content -->
+							</li>
+							<li class="feature">
+								<div class="feature-thumbnail">
+									<div class="circle-icon">
+										<div>
+											<i class="icon-file"></i>
+										</div>
+									</div> <!-- /circle-icon -->
+								</div> <!-- /feature-thumbnail -->
+								
+								<div class="feature-content">
+									<h3>Feature #6</h3>
+									<p>Phasellus varius sapien id ante ultrices eu malesuada mauris molestie. Cras erat tellus arcu  mi eget adipiscing quam.</p>
+							</div> <!-- /feature-content -->
+							</li>
+						</ul> <!-- /features-list -->
+						 <!-- /features-list -->
+					
+				</div> <!-- /span8 -->
+			
+				
+				<div class="span4">
+				
+						<h3><span class="slash">//</span> Testimonials</h3>
+						
+						
+						<ul class="testimonials-list">
+							
+							<li>
+								<div class="testimonial-avatar">
+									<div class="img">
+										<img alt="Thumbnail" src="http://wbpreview.com/previews/WB000C138/img/avatar.png">									
+									</div>
+								</div> <!-- /testimonial-avatar -->
+								
+								<div class="testimonial-text">
+									<p>
+										<span class="testimonial-author">John Doe</span>
+										Morbi a tellus tellus. Integer et ipsum ipsum, blandit pharetra nisl. In vitae lorem a magna.
+										
+									</p>								
+								</div> <!-- /testimonial-text -->
+							</li>
+							
+							<li>
+								<div class="testimonial-avatar">
+									<div class="img">
+										<img alt="Thumbnail" src="http://wbpreview.com/previews/WB000C138/img/avatar2.png">									
+									</div>
+								</div> <!-- /testimonial-avatar -->
+								
+								<div class="testimonial-text">
+									<p>
+										<span class="testimonial-author">John Doe</span>
+										Morbi a tellus tellus. Integer et ipsum ipsum, blandit pharetra nisl. In vitae lorem a magna.
+										
+									</p>								
+								</div> <!-- /testimonial-text -->
+							</li>
+								
+								
+							<li>
+								<div class="testimonial-avatar">
+									<div class="img">
+										<img alt="Thumbnail" src="http://wbpreview.com/previews/WB000C138/img/avatar3.png">									
+									</div>
+								</div> <!-- /testimonial-avatar -->
+								
+								<div class="testimonial-text">
+									<p>
+										<span class="testimonial-author">John Doe</span>
+										Morbi a tellus tellus. Integer et ipsum ipsum, blandit pharetra nisl. In vitae lorem a magna.
+										
+									</p>								
+								</div> <!-- /testimonial-text -->
+							</li>
+							
+						</ul>	
+					
+				</div> <!-- /span4 -->
+				
+			</div> <!-- /row -->			
+			
+		</div> <!-- /container -->	
+		
+	</div> <!-- /inner -->
+	
+</div><!-- Content -->
+
 <!-- SPAN4 subiu pra SPAN12 somente para apresentar o icone do Facebook-->
 <!-- <div class="span4">
 
