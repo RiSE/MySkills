37bea03006af3731ee043bb1cbcaf9d16394a227
Tiago Perrelli <tiagoyg@gmail.com>
2012-08-13 14:03:05 -0300

    code identation
    code identation

--- application/models/badge_model.php
+++ application/models/badge_model.php
@@ -49,102 +49,102 @@ class Badge_model extends CI_Model {
 
         return $result;
     }
-    
+
     public function listBadgesProfessional($idUser, $idBadge) {
-        
+
         $result = array();
-        
+
         $this->db->select('id_user, id_badge, active');
         $this->db->where('id_user', $idUser);
         $this->db->where('id_badge', $idBadge);
-        
-        
+
         $query = $this->db->get('badges_user');
-        
+
         if ($query->num_rows() > 0) {
             $result = $query->result_object();
         }
-        
-        return $result;
-    }
-    
-    public function listBadgesProfessionalByProfessional($idUser) {
-        
-        $result = array();
-        
-        $this->db->select('id_user, id_badge, active');
-        $this->db->where('id_user', $idUser);
-        
-        $query = $this->db->get('badges_user');
-        
-        if ($query->num_rows() > 0) {
-            $result = $query->result_object();
-        }
-        
-        return $result;
-    }
-    
-    public function listBadgesProfessionalByUser($idUser) {
-        
-        $result = array();
-        
-        $this->db->select('id_user, id_badge, active');
-        $this->db->where('id_user', $idUser);
-        
-        $query = $this->db->get('badges_user');
-        
-        if ($query->num_rows() > 0) {
-            $result = $query->result_object();
-        }
-        
+
         return $result;
     }
 
- public  function getImgBadgs($idBadges){
-    	$img = "";
-    	switch($idBadges){
-    		case 1:
-    			 $img = "iOSBadge100.png";
-    		break;
-    		case 2:
-    			 $img = "php.png";
-    		break;
-    		case 3:
-    			 $img = "androidbadges.png";
-    		break;
-    		case 4:
-    			 $img = "php.png";
-    		break;
-    		case 5:
-    			 $img = "campus-badge.png";
-    		break;
-    		case 6:
-    			 $img = "java-badge.png";
-    		break;
-    		case 7:
-    			 $img = "php5-badge.png";
-    		break;
-    		case 8:
-    			 $img = "php53-badge.png";
-    		break;
-    		case 9:
-    			 $img = "tryruby-badge.png";
-    		break;
-    		case 10:
-    			 $img = "trygit-badge.png";
-    		break;
-    		/*case 1:
-    			 $img = "iOSBadge100.png";
-    		break;
-    		case 1:
-    			 $img = "iOSBadge100.png";
-    		break;
-    		case 1:
-    			 $img = "iOSBadge100.png";
-    		break;*/
-    	}
-    	return $img;
+    public function listBadgesProfessionalByProfessional($idUser) {
+
+        $result = array();
+
+        $this->db->select('id_user, id_badge, active');
+        $this->db->where('id_user', $idUser);
+
+        $query = $this->db->get('badges_user');
+
+        if ($query->num_rows() > 0) {
+            $result = $query->result_object();
+        }
+
+        return $result;
     }
+
+    public function listBadgesProfessionalByUser($idUser) {
+
+        $result = array();
+
+        $this->db->select('id_user, id_badge, active');
+        $this->db->where('id_user', $idUser);
+
+        $query = $this->db->get('badges_user');
+
+        if ($query->num_rows() > 0) {
+            $result = $query->result_object();
+        }
+
+        return $result;
+    }
+
+    public function getImgBadgs($idBadges) {
+        $img = "";
+        switch ($idBadges) {
+            case 1:
+                $img = "iOSBadge100.png";
+                break;
+            case 2:
+                $img = "php.png";
+                break;
+            case 3:
+                $img = "androidbadges.png";
+                break;
+            case 4:
+                $img = "php.png";
+                break;
+            case 5:
+                $img = "campus-badge.png";
+                break;
+            case 6:
+                $img = "java-badge.png";
+                break;
+            case 7:
+                $img = "php5-badge.png";
+                break;
+            case 8:
+                $img = "php53-badge.png";
+                break;
+            case 9:
+                $img = "tryruby-badge.png";
+                break;
+            case 10:
+                $img = "trygit-badge.png";
+                break;
+            /* case 1:
+              $img = "iOSBadge100.png";
+              break;
+              case 1:
+              $img = "iOSBadge100.png";
+              break;
+              case 1:
+              $img = "iOSBadge100.png";
+              break; */
+        }
+        return $img;
+    }
+
     public function insertBadgeProfessional($data = array()) {
 
         $this->db->trans_start();
