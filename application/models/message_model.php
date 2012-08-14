aa86ab79e1da5c5fc1106a48a60370c1a0c221bd
eliakim ramos de souza <eliakim.ramos@rise.com.br>
2012-08-14 18:19:25 -0300

    add functionality od add message in dashboard. add file message_model.php
    add functionality od add message in dashboard. add file
    message_model.php

---
+++ application/models/message_model.php
@@ -0,0 +1,47 @@
+<?php
+
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
+
+/**
+ * Description of badge_model
+ *
+ * @author Eliakim Ramos <Eliakim.Ramos at www.myskills.com>
+ */
+class Message_model extends CI_Model {
+
+    private $table;
+
+    public function __construct() {
+        parent::__construct();
+
+        $this->table = 'message';
+    }
+
+    public function listMessages() {
+
+        $result = array();
+
+        $this->db->select('id_message, message, created, id_user');
+        $this->db->order_by('created', 'DESC');
+
+        $query = $this->db->get($this->table);
+
+        if ($query->num_rows() > 0) {
+            $result = $query->result_object();
+        }
+
+        return $result;
+    }
+
+
+    public function insertMessage($data = array()) {
+
+        $this->db->trans_start();
+        $this->db->insert('message', $data);
+        $this->db->trans_complete();
+    }
+
+}
+
+?>
