3b354854877d245c083c2ec92b6b3a040670ffc7
eliakim ramos de souza <eliakim.ramos@rise.com.br>
2012-08-29 18:57:02 -0300

    adjust edit profile
    adjust edit profile

--- application/views/index/editProfile.php
+++ application/views/index/editProfile.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 $fbuid = $this->session->userdata('uid');
 $arrBlockedIds = array('100000634528702', '578648267', '1781396621');
@@ -19,88 +18,55 @@ if (!in_array($fbuid, $arrBlockedIds) && $_SERVER['HTTP_HOST'] != 'localhost') :
 </div>
 
 <div id="subpage">	
-    <div class="container">
+ <div class="container">
+	 <div class="row">
+			  <div class="span9">
+			  		<?php if ($this->session->flashdata('error_message') != '') : ?>
+		                <div class="alert alert-error">
+		                    <button type="button" class="close" data-dismiss="alert">×</button>
+		                    <strong>Oh snap!</strong> <?php echo $this->session->flashdata('error_message'); ?>
+		                </div>
+            		<?php endif; ?>
 
-        <form method="POST" name="frmEditProfile">
-
-            <?php if ($this->session->flashdata('error_message') != '') : ?>
-                <div class="alert alert-error">
-                    <button type="button" class="close" data-dismiss="alert">×</button>
-                    <strong>Oh snap!</strong> <?php echo $this->session->flashdata('error_message'); ?>
-                </div>
-            <?php endif; ?>
-
-            <?php if ($this->session->flashdata('success_message') != '') : ?>
-                <div class="alert alert-success">
-                    <button type="button" class="close" data-dismiss="alert">×</button>
-                    <strong>Well done!</strong> <?php echo $this->session->flashdata('success_message'); ?>
-                </div>
-            <?php endif; ?>
-
-            <div class="row-fluid">
-
-                <div class="span9">
-
-                    <!--<img id="userpic" src="https://graph.facebook.com/<?php echo $this->session->userdata('uid'); ?>/picture&type=normal"  style="border:thick groove green;" />-->
-
-                    <!--<div class="row">
-                        <div class="span6">
-                            <span class="label label-info">Name</span>
-                            <input type="text" >
-                        </div>
-                    </div>-->
-                    <div class="row">
-                        <div class="span6">
-                            <span class="label label-info">Name</span>
-                            <input type="text" class="span4" name="name" value="<?php echo $user->name; ?>"/>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="span6">
-                            <span class="label label-info">Surname</span>
-                            <input type="text" class="span4" name="surname" value="<?php echo $user->surname; ?>"/>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="span6">
-                            <span class="label label-info">E-mail</span>
-                            <input type="text" class="span4" name="email" value="<?php echo $user->email; ?>"/>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="span6">
-                            <span class="label label-info">I want a position as:</span>
-                            <label class="checkbox">
+		            <?php if ($this->session->flashdata('success_message') != '') : ?>
+		                <div class="alert alert-success">
+		                    <button type="button" class="close" data-dismiss="alert">×</button>
+		                    <strong>Well done!</strong> <?php echo $this->session->flashdata('success_message'); ?>
+		                </div>
+		            <?php endif; ?>
+			    <form method="POST" name="frmEditProfile">
+			   	    <legend>Legend</legend>
+					  <label>Name</label>
+					  <input type="text" placeholder="Name" class="span4" name="name" value="<?php echo $user->name; ?>">
+					  <label>Last name</label>
+					  <input type="text" placeholder="Last name" class="span4" name="surname" value="<?php echo $user->surname; ?>">
+					  <label>E-mail</label>
+					  <input type="text" placeholder="E-mail" class="span4" name="email" value="<?php echo $user->email; ?>" >
+					  <label>I want a position as:</label>
+					  		<label class="checkbox inline">
 							  <input type="checkbox" name="trainee" <?php if($user->trainee == "1"){ echo "checked = 'checked'";}?> value="1">
 							 Trainee
 							</label>
-                            <label class="checkbox">
+                            <label class="checkbox inline">
 							  <input type="checkbox" name="employee" <?php if($user->employee == "1"){ echo "checked = 'checked'";}?> value="1">
 							  Employee
 							</label>
-                            <label class="checkbox">
+                            <label class="checkbox inline">
 							  <input type="checkbox" name="freelancer" <?php if($user->freelancer == "1"){ echo "checked = 'checked'";}?> value="1">
 							  Freelancer
 							</label>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="span6">
-                            <span class="label label-info">Availability of working::</span>
-                            <label class="checkbox">
+					  
+					  <label>Availability of working:</label>
+					  <label class="checkbox inline">
 							  <input name="anotherCity" type="checkbox" <?php if($user->another_city == "1"){ echo "checked = 'checked'";}?> value="1">
 							 I want to work in Another City
 							</label>
-							<label class="checkbox">
+							<label class="checkbox inline">
 							  <input type="checkbox" name="anotherCountry" <?php if($user->another_country == "1"){ echo "checked = 'checked'";}?> value="1">
 							 I want to work in Another Country
 							</label>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="span6">
-                          <span class="label label-info">State</span>
-                           <select name="state">
+					  <label>State</label>
+					  		<select name="state">
 							  <option value="">select your state</option>
 							  <option value="AC" <?php if($user->state == "AC"){ echo "selected = 'selected'";}?> >AC</option>
 							  <option value="AL" <?php if($user->state == "AL"){ echo "selected = 'selected'";}?>>AL</option>
@@ -129,39 +95,29 @@ if (!in_array($fbuid, $arrBlockedIds) && $_SERVER['HTTP_HOST'] != 'localhost') :
 							  <option value="SE" <?php if($user->state == "SE"){ echo "selected = 'selected'";}?>>SE</option>
 							  <option value="TO" <?php if($user->state == "TO"){ echo "selected = 'selected'";}?>>TO</option>
 							</select>
-                            
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="span6">
-                            <span class="label label-info">Video-url</span>
-                            <input type="text" class="span4" name="video_url" value="<?php echo $user->video_url; ?>"/>
-                        </div>
-                    </div>
-                                        
-                    <div class="row">
-                        <div class="span1">
-                            <input type="submit" class="btn btn-primary btn-large" value="Save" />
-                        </div>                        
-                    </div>
+					  <label>Video-url</label>
+					  <input type="text" placeholder="video_url" class="span4" name="video_url" value="<?php echo $user->video_url; ?>">
+					  
+					  <button type="submit" class="btn">Submit</button>
+				</form>
+			  </div><!-- span9 -->
+			      <div class="span3">
+			      	   <div class="sidebar">
+	                        <h4>
+	                        	<span class="slash">Applicants (Coming Soon)</span>
+	                        </h4><!-- h4 -->
+	                        <p>Are you the only one who wants to work in this company? Find out here.</p>
+	                        <h4>
+	                        	<span class="slash">Skill Points (Coming Soon)</span>
+	                        </h4><!-- h4 -->
+	                        <p>The developers in this company are skilled? Here you find how many skill points this company has.</p>
+	                        <h4>
+	                        	<span class="slash">Wishlist (Coming Soon)</span>
+	                        </h4><!-- h4 -->
+	                        <p>Do you wish to work in a specific company? Add it to your Wishlist and we might help you.</p>
 
-                </div>
-
-                <div class="span3">
-                    <div class="sidebar">
-                        <h4><span class="slash">Applicants (Coming Soon)</span></h4>
-                        <p>Are you the only one who wants to work in this company? Find out here.</p>
-
-                        <h4><span class="slash">Skill Points (Coming Soon)</span></h4>
-                        <p>The developers in this company are skilled? Here you find how many skill points this company has.</p>
-                        <h4><span class="slash">Wishlist (Coming Soon)</span></h4>
-                        <p>Do you wish to work in a specific company? Add it to your Wishlist and we might help you.</p>
-
-                    </div>
-                </div>
-
-            </div>
-
-        </form>
-    </div>
-</div>
+		               </div><!-- sidebar -->
+			      </div><!-- span3 -->
+	 </div><!-- row -->
+</div> <!-- container -->
+</div><!-- Subpage -->
