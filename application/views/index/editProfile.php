0afd890e65bc46e1f39f3975863ed3c26f68f453
eliakim ramos de souza <eliakim.ramos@rise.com.br>
2012-08-30 11:44:57 -0300

    adjust edit profile: edit html
    adjust edit profile: edit html

--- application/views/index/editProfile.php
+++ application/views/index/editProfile.php
@@ -36,50 +36,82 @@ if (!in_array($fbuid, $arrBlockedIds) && $_SERVER['HTTP_HOST'] != 'localhost') :
 		            <?php endif; ?>
 			    <form method="POST" class="form-horizontal" name="frmEditProfile">
 			   	    <legend>Legend</legend>
-					  <label>First Name</label>
-					  <input type="text" placeholder="First Name" class="span4" name="name" value="<?php echo $user->name; ?>"/>
-					  <label>Last name</label>
-					  <input type="text" placeholder="Last name" class="span4" name="surname" value="<?php echo $user->surname; ?>"/>
-					  <label>E-mail</label>
-					  <span class="add-on"><i class="icon-envelope"></i><span><input type="text" placeholder="E-mail" class="span4" name="email" value="<?php echo $user->email; ?>" />
-					  <label>I want a position as:</label>
-					  		<label class="checkbox inline">
-							  <input type="checkbox" name="trainee" <?php if($user->trainee == "1"){ echo "checked = 'checked'";}?> value="1"/>
-							 Trainee
-							</label>
-                            <label class="checkbox inline">
-							  <input type="checkbox" name="employee" <?php if($user->employee == "1"){ echo "checked = 'checked'";}?> value="1"/>
-							  Employee
-							</label>
-                            <label class="checkbox inline">
-							  <input type="checkbox" name="freelancer" <?php if($user->freelancer == "1"){ echo "checked = 'checked'";}?> value="1"/>
-							  Freelancer
-							</label>
-					  
-					  <label>I am avaliable to work in:</label>
-					  <label class="checkbox inline">
-							  <input name="anotherCity" type="checkbox" <?php if($user->another_city == "1"){ echo "checked = 'checked'";}?> value="1"/>
-							 Another City
-							</label>
-							<label class="checkbox inline">
-							  <input type="checkbox" name="anotherCountry" <?php if($user->another_country == "1"){ echo "checked = 'checked'";}?> value="1"/>
-							  Another Country
-							</label>
-					  <label>State</label>
-					    	<select name="state">
-							  <option value="">select your state</option>
-							   <?php foreach ($state as $stateDados){ ?>
-							   		<option value="<?php echo $stateDados->id_state;?>" <?php if($user->state == $stateDados->id_state){ echo "selected = 'selected'";}?>><?php echo $stateDados->name; ?></option>
-							   	<?php } ?>
-							</select>
-					  <label>1 minute introduction (youtube):</label>
-					  <input type="text" placeholder="http://www.youtube.com/watch?v=" class="span4" name="video_url" value="<?php echo $user->video_url; ?>"/>
-					  <label>Vizify portfolio:</label>
-					  <input type="text" placeholder="http://www.vizify.com/your-username" class="span4" name="vizify_portfolio" value="<?php echo $user->vizify_portfolio; ?>"/>
-					  
-					  <label>
-					  	<button type="submit" class="btn btn-primary">Submit</button>
-					  </label>
+					 <div class="control-group">
+					  	<label class="control-label">First Name</label>
+					  	<div class="controls">
+					  		<input type="text" placeholder="First Name" class="span4" name="name" value="<?php echo $user->name; ?>"/>
+					  	</div><!-- controls -->
+					  </div><!-- control-group -->
+					  <div class="control-group">
+					  		<label class="control-label">Last name</label>
+					  		<div class="controls">
+					  			<input type="text" placeholder="Last name" class="span4" name="surname" value="<?php echo $user->surname; ?>"/>
+					  		</div><!-- controls -->
+					  </div><!-- control-group -->
+					  <div class="control-group">
+					  		<label class="control-label">E-mail</label>
+					  		<div class="controls">
+					  			<span class="add-on"><i class="icon-envelope"></i><span><input type="text" placeholder="E-mail" class="span4" name="email" value="<?php echo $user->email; ?>" />
+					  		</div><!-- controls -->
+					  </div><!-- control-group -->
+					  <div class="control-group">
+					  		<label class="control-label">I want a position as:</label>
+					  		<div class="controls">
+						  		<label class="checkbox inline">
+								  <input type="checkbox" name="trainee" <?php if($user->trainee == "1"){ echo "checked = 'checked'";}?> value="1"/>
+								 Trainee
+								</label>
+	                            <label class="checkbox inline">
+								  <input type="checkbox" name="employee" <?php if($user->employee == "1"){ echo "checked = 'checked'";}?> value="1"/>
+								  Employee
+								</label>
+	                            <label class="checkbox inline">
+								  <input type="checkbox" name="freelancer" <?php if($user->freelancer == "1"){ echo "checked = 'checked'";}?> value="1"/>
+								  Freelancer
+								</label>
+							</div><!-- controls -->
+					  </div><!-- control-group -->
+					  <div class="control-group">
+					  		<label class="control-label">I am avaliable to work in:</label>
+					  		<div class="controls">
+								  <label class="checkbox inline">
+										  <input name="anotherCity" type="checkbox" <?php if($user->another_city == "1"){ echo "checked = 'checked'";}?> value="1"/>
+										 Another City
+										</label>
+										<label class="checkbox inline">
+										  <input type="checkbox" name="anotherCountry" <?php if($user->another_country == "1"){ echo "checked = 'checked'";}?> value="1"/>
+										  Another Country
+										</label>
+							</div><!-- controls -->
+					  </div><!-- control-group -->
+					  <div class="control-group">
+					  		<label class="control-label">State</label>
+					  		<div class="controls">
+						    	<select name="state">
+								  <option value="">select your state</option>
+								   <?php foreach ($state as $stateDados){ ?>
+								   		<option value="<?php echo $stateDados->id_state;?>" <?php if($user->state == $stateDados->id_state){ echo "selected = 'selected'";}?>><?php echo $stateDados->name; ?></option>
+								   	<?php } ?>
+								</select>
+							</div><!-- controls -->
+					  </div><!-- control-group -->
+					  <div class="control-group">
+					  		<label class="control-label">1 minute introduction (youtube):</label>
+					  		<div class="controls">
+					  			<input type="text" placeholder="http://www.youtube.com/watch?v=" class="span4" name="video_url" value="<?php echo $user->video_url; ?>"/>
+					  		</div><!-- controls -->
+					  </div><!-- control-group -->
+					   <div class="control-group">
+					  		<label class="control-label">Vizify portfolio:</label>
+					  		<div class="controls">
+					  			<input type="text" placeholder="http://www.vizify.com/your-username" class="span4" name="vizify_portfolio" value="<?php echo $user->vizify_portfolio; ?>"/>
+					  		</div><!-- controls -->
+					  </div><!-- control-group -->
+					  <div class="control-group">
+    					<div class="controls">
+					  		<button type="submit" class="btn btn-primary">Submit</button>
+					  	</div><!-- controls -->
+					  </div><!-- control-group -->
 				</form>
 			  </div><!-- span9 -->
 			      <div class="span3">
